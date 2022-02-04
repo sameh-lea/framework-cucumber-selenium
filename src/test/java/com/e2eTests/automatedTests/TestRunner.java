d75b91ebc32449137f7240ef2d2575fac92d2a2b
sameh-lea <ferchichisamehfsa@gmail.com>
2022-02-04 22:07:35 +0100

    first commit

---
+++ src/test/java/com/e2eTests/automatedTests/TestRunner.java
@@ -0,0 +1,5 @@
+package com.e2eTests.automatedTests;
+
+public class TestRunner {
+
+}
---
+++ src/test/java/com/e2eTests/automatedTests/authentication/pageObjects/AuthenticationPage.java
@@ -0,0 +1,5 @@
+package com.e2eTests.automatedTests.authentication.pageObjects;
+
+public class AuthenticationPage {
+
+}
